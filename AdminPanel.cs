4367f8ce808118ef47bbe3551a990dc3dffe8d44
ROCKET <ROCKET@ROCKET-PC>
2023-11-08 19:29:37 +0800

    Add project files.

---
+++ AdminPanel.cs
@@ -0,0 +1,10 @@
+namespace Pocket_Audior_Admin_Panel
+{
+    public partial class AdminPanel : Form
+    {
+        public AdminPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
