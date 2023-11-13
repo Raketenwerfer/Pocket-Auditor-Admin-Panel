5834420d5182c36f698599900a09f85543e758ac
ROCKET <ROCKET@ROCKET-PC>
2023-11-13 14:33:00 +0800

    Added contents for mdl_SubIndicators

--- Classes/mdl_SubIndicators.cs
+++ Classes/mdl_SubIndicators.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Pocket_Audior_Admin_Panel.Classes
 {
-    internal class mdl_SubIndicators
+    public class mdl_SubIndicators
     {
+        public int SubIndicatorID { get; set; }
+        public string SubIndicator { get; set; }
+        public string SubIndicatorType { get; set; }
+        public string SubIndicatorStatus { get; set; }
+        public double ScoreValue { get; set; }
+
+        public mdl_SubIndicators(int subIndicatorID, string subIndicator, string subIndicatorType, string subIndicatorStatus, double scoreValue)
+        {
+            SubIndicatorID = subIndicatorID;
+            SubIndicator = subIndicator;
+            SubIndicatorType = subIndicatorType;
+            SubIndicatorStatus = subIndicatorStatus;
+            ScoreValue = scoreValue;
+        }
     }
 }
