f71ba16d008ef7c0afac26df9b876198a0e073e4
ROCKET <ROCKET@ROCKET-PC>
2023-11-13 14:41:53 +0800

    Added contents for mdl_Indicators

--- Classes/mdl_Indicators.cs
+++ Classes/mdl_Indicators.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Pocket_Audior_Admin_Panel.Classes
 {
-    internal class mdl_Indicators
+    public class mdl_Indicators
     {
+        public int IndicatorID { get; set; }
+        public int IndicatorNumber { get; set; }
+        public double ScoreValue { get; set; }
+        public string Indicator { get; set; }
+        public string IndicatorStatus { get; set; }
+        public string IndicatorType { get; set; }
+
+        public mdl_Indicators(int indicatorID, int indicatorNumber, double scoreValue, string indicator, string indicatorStatus, string indicatorType)
+        {
+            IndicatorID = indicatorID;
+            IndicatorNumber = indicatorNumber;
+            ScoreValue = scoreValue;
+            Indicator = indicator;
+            IndicatorStatus = indicatorStatus;
+            IndicatorType = indicatorType;
+        }
     }
 }
