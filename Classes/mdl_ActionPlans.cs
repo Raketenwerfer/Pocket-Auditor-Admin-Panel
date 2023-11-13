c5584a078e50c9119c8f433b70c978d326a7a5b8
ROCKET <ROCKET@ROCKET-PC>
2023-11-13 14:51:31 +0800

    Added contents for mdl_ActionPlans

--- Classes/mdl_ActionPlans.cs
+++ Classes/mdl_ActionPlans.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Pocket_Audior_Admin_Panel.Classes
 {
-    internal class mdl_ActionPlans
+    public class mdl_ActionPlans
     {
+        public int ActionPlanID { get; set; }
+        public string ActionPlanName { get; set; }
+        public string ActionPlanDetails { get; set; }
+        public string ActionPlanType { get; set; }
+        public string ActionPlanStatus { get; set; }
+
+        public mdl_ActionPlans(int actionPlanID, string actionPlanName, string actionPlanDetails, string actionPlanType, string actionPlanStatus)
+        {
+            ActionPlanID = actionPlanID;
+            ActionPlanName = actionPlanName;
+            ActionPlanDetails = actionPlanDetails;
+            ActionPlanType = actionPlanType;
+            ActionPlanStatus = actionPlanStatus;
+        }
     }
 }
