7c60bb7e30c207e99d01e955f0054f2c8e070e5b
ROCKET <ROCKET@ROCKET-PC>
2023-11-13 14:45:08 +0800

    Added contents for mdl_ChapterScores

--- Classes/mdl_ChapterScores.cs
+++ Classes/mdl_ChapterScores.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Pocket_Audior_Admin_Panel.Classes
 {
-    internal class mdl_ChapterScores
+    public class mdl_ChapterScores
     {
+        public int ChapterID_fk { get; set; }
+        public int CategoryID_fk { get; set; }
+        public double Score { get; set; }
+
+        public mdl_ChapterScores(int chapterID_fk, int categoryID_fk, double score)
+        {
+            ChapterID_fk = chapterID_fk;
+            CategoryID_fk = categoryID_fk;
+            Score = score;
+        }
     }
 }
